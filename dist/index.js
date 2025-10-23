8a54afa6b229f8b6f3072d2265bb4902cb8a6074
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2025-10-23 16:05:21 +0200

    Add CSS and JS with theme

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+/**
+ * Automatically set mode on preferred color change
+ */
+const detectMode = () => {
+    if (window.matchMedia('(prefers-color-scheme: dark)')) {
+        setMode("dark");
+    };
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        const mode = event.matches ? "dark" : "light";
+        setMode(mode);
+    });
+}
+
+/**
+ * Set color mode
+ */
+const setMode = (mode) => {
+    const dataset = document.querySelector("body").dataset;
+    console.log(dataset)
+    dataset.mode = mode;
+};
+
+/**
+ * Run main code
+ */
+const init = () => {
+    console.log(document.querySelector("body"))
+    detectMode();
+};
+
+/**
+ * Execute script on DOM ready
+ */
+document.addEventListener("DOMContentLoaded", init);
