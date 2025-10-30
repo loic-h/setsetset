e05bd69d3391b7ab7e90e1107cd38b7f57a85591
Loïc Hamet <1453246+loic-h@users.noreply.github.com>
2025-10-30 19:39:16 +0100

    Section background colors

--- dist/js/utils/initActiveAnchor.js
+++ dist/js/utils/initActiveAnchor.js
@@ -15,15 +15,21 @@ export default function initActiveAnchor () {
 };
 
 function handleScroll(items) {
+    let hasActive = false;
     items.forEach(({ anchor, section }) => {
         const anchorRect = anchor.getBoundingClientRect();
         const sectionRect = section.getBoundingClientRect();
         if(anchorRect.bottom === sectionRect.top) {
             anchor.setAttribute("active", "");
+            document.querySelector("body").setAttribute("data-active", anchor.getAttribute("data-anchor"));
+            hasActive = true;
         } else {
             anchor.removeAttribute("active");
         }
     });
+    if (!hasActive) {
+        document.querySelector("body").setAttribute("data-active", "1")
+    }
 }
 
 function getAnchorNextSection (anchor) {
@@ -38,8 +44,8 @@ function debounce(callback, delay = 0){
     let timer;
     return (...args) => {
         clearTimeout(timer);
-        timer = setTimeout(() => { 
-            callback.apply(this, args); 
+        timer = setTimeout(() => {
+            callback.apply(this, args);
         }, delay);
     };
 }
